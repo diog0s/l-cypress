75431fd49e2b7e879ddc5d8b1fe2cc59cc8123f2
Diogo <oxediogo25@gmail.com>
2021-11-07 12:10:28 -0300

    refatorando

--- cypress/integration/loja/escolher_vestido.spec.js
+++ cypress/integration/loja/escolher_vestido.spec.js
@@ -1,4 +1,17 @@
 /// <reference types="cypress"/>
-context('escolher um vestido', () => {
-    before
+context('vestido', () => {
+    beforeEach('Acessando loja', () =>{
+        cy.visit('http://automationpractice.com/index.php')
+    });
+    it('escolher um vestido', () =>{
+        cy.get('[class=login]').click()
+        cy.get('[id=email]').type('diogotest@gmail.com',{delay: 10})
+        cy.get('[id=passwd]').type('12345678',{delay: 10})
+        cy.get('[id="SubmitLogin"]').click()
+
+        cy.get('.sf-menu > :nth-child(2) > .sf-with-ul').click()
+        cy.get('.first-in-line.first-item-of-tablet-line > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()
+       // cy.get('.button-container > .button-medium > span').click()
+        cy.get('.layer_cart_product > h2').contains('Product successfully added to your shopping cart')
+    });
 });
--- cypress/integration/loja/login.spec.js
+++ cypress/integration/loja/login.spec.js
@@ -18,20 +18,7 @@ context('Loja de roupas', () => {
         cy.get('[id="SubmitLogin"]').click()
         cy.get('ol > li').contains('Authentication failed.')
     });
-
-    it.skip('escolher um dress', () => {
-        cy.get('[class=login]').click()
-        cy.get('[id=email]').type('diogotest@gmail.com',{delay: 10})
-        cy.get('[id=passwd]').type('12345678',{delay: 10})
-        cy.get('[id="SubmitLogin"]').click()
-
-        cy.get('.sf-menu > :nth-child(2) > .sf-with-ul').click()
-        cy.get('.first-in-line.first-item-of-tablet-line > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()
-       // cy.get('.button-container > .button-medium > span').click()
-        cy.get('.layer_cart_product > h2').contains('Product successfully added to your shopping cart')
-
-    });
-
+s
     it.skip('comprar um dress path 2', () => {
         cy.get('[class=login]').click()
         cy.get('[id=email]').type('diogotest@gmail.com',{delay: 10})
